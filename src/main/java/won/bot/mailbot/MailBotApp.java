53dad9493ca8701cd70043b692ffb42610065c03
maxstolze <maximilian.stolze@researchstudio.at>
2019-10-07 12:17:42 +0200

    Adjist pom.xml and MailBotApp.java

--- src/main/java/won/bot/mailbot/MailBotApp.java
+++ src/main/java/won/bot/mailbot/MailBotApp.java
@@ -3,12 +3,19 @@ package won.bot.mailbot;
 import org.springframework.boot.SpringApplication;
 import won.bot.framework.bot.utils.BotUtils;
 
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootConfiguration
+@PropertySource("classpath:application.properties")
+@ImportResource("classpath:/spring/app/botApp.xml")
 public class MailBotApp {
     public static void main(String[] args) {
         if (!BotUtils.isValidRunConfig()) {
             System.exit(1);
         }
-        SpringApplication app = new SpringApplication("classpath:/spring/app/botApp.xml");
+        SpringApplication app = new SpringApplication(MailBotApp.class);
         app.setWebEnvironment(false);
         app.run(args);
         // ConfigurableApplicationContext applicationContext = app.run(args);
